1c320618999c125923f4f63d29279ce8ed893cb9
Amit Mishra20 <amittttmishra@gmail.com>
2019-11-29 01:25:05 +0530

    Adding more code

--- flutterlistwithnoandarrow/lib/main.dart
+++ flutterlistwithnoandarrow/lib/main.dart
@@ -3,84 +3,132 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Application',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(title: Text('DES 0803 Stats with Recomm')),
+        body: BodyLayout(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class BodyLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return _myListView(context);
   }
+}
+
+// replace this function with the code in the examples
+Widget _myListView(BuildContext context) {
+  final titles = [
+    '4xx Errors',
+    '5xx Errors',
+    'CPU Usage',
+    'Traffic',
+    'Failed Transaction',
+    'Unexpected APP Pool',
+    'CPU Crosses',
+    'Memory Crosses',
+    'Load in KB',
+    'DB Timeouts',
+    'Service Response Time',
+    'Service Transactions'
+  ];
+  final scores = [
+    '22%',
+    '33%',
+    '123%',
+    '43%',
+    '55%',
+    '444%',
+    '23%',
+    '123%',
+    '533%',
+    '55%',
+    '2%',
+    '10%'
+  ];
+
+  final iconList =[
+    'arrow_downward',
+    'arrow_upward',
+    'arrow_upward',
+    'arrow_upward',
+    'arrow_upward',
+    'arrow_downward',
+    'arrow_upward',
+    'arrow_upward',
+    'arrow_upward',
+    'arrow_upward',
+    'arrow_upward',
+    'arrow_upward'
+  ];
+
+  return ListView.builder(
+    itemCount: titles.length,
+    itemBuilder: (context, index) {
+      return Container(
+        height: 70,
+        child: Card(
+            child: new Container(
+          child: Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 10.0),
+                //color: Colors.blue,
+                height: 70.0,
+                width: 280.0,
+                child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    new Text(
+                      titles[index],
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 4.0),
+                //color: Colors.red,
+                width: 60.0,
+                child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    new Text(
+                      scores[index],
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 2.0),
+                //color: Colors.red,
+                width: 40.0,
+                child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    iconList[index]=="arrow_upward"?Icon(Icons.arrow_upward, size: 40, color: Colors.red.shade400,):Icon(Icons.arrow_downward, size: 40, color: Colors.green.shade400,),
+                    
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )),
+      );
+    },
+  );
 }
